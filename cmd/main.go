0fa05bde0b3995d94190e028d38772a565430a7f
abbottme <abbottme@student.21-school.ru>
2026-06-19 16:43:31 +0900

    added swagger doc in main

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,11 @@ import (
 	"go.uber.org/fx"
 )
 
+// @title           Subscription Aggregator API
+// @version         1.0
+// @description     REST-сервис агрегации онлайн-подписок
+// @host            localhost:8080
+// @BasePath        /
 func main() {
 	fx.New(di.Module()).Run()
 }
